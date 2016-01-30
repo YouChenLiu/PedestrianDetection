52e94903b68cef7da5e72b481ff4787d7223f5c7
YouChen Liu <youchenliu@outlook.com>
2016-01-31 00:54:00 +0800

    test mySVM module

--- test/Source.cpp
+++ test/Source.cpp
@@ -1,7 +1,15 @@
 #include <opencv2/core.hpp>
-#include "../myLibrary/myFeatureExtractor/myLBP/myLBP.h"
+#include "../myLibrary/myClassifier/mySVM/mySVM.h"
 
 int main(void) {
-	myLBP LBP(cv::Mat(), myLBP::Feature::LBP_8_1_UNIFORM);
+    mySVM oSVM;
+    oSVM.AddSample(1, std::vector<float>(10, 1.0f));
+    oSVM.AddSample(-1, std::vector<float>(10, -1.0f));
+    oSVM.Train();
+
+    auto result = oSVM.Predict(std::vector<float>(10, -1.0f));
+    oSVM.Save("test.xml");
+    oSVM.Load("test.xml");
+    result = oSVM.Predict(std::vector<float>(10, 1.0f));
 	return 0;
 }
