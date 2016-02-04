d1dfcfdf2328509603669f58a21f8781c08ba8a8
YouChen Liu <youchenliu@outlook.com>
2016-02-04 09:48:58 +0800

    AdaBoost is complete

--- myLibrary/myClassifier/myAdaBoost/myAdaBoost.cpp
+++ myLibrary/myClassifier/myAdaBoost/myAdaBoost.cpp
@@ -1,8 +1,16 @@
 #include "myAdaBoost.h"
 
+const double myAdaBoost::TRIM_RATE = 0.95;
 
+myAdaBoost::myAdaBoost(unsigned int iWeakCount) {
+    // create adaboost
+    auto pBoost = cv::ml::Boost::create();
+    m_poClassifier = pBoost;
 
-myAdaBoost::myAdaBoost() {}
+    // set parameter
+    pBoost->setBoostType(myAdaBoost::BOOST_TYPE);
+    pBoost->setWeightTrimRate(myAdaBoost::TRIM_RATE);
+    pBoost->setWeakCount(iWeakCount);
+}
 
-
-myAdaBoost::~myAdaBoost() {}
+myAdaBoost::~myAdaBoost(void) {}
--- myLibrary/myClassifier/myAdaBoost/myAdaBoost.h
+++ myLibrary/myClassifier/myAdaBoost/myAdaBoost.h
@@ -3,10 +3,29 @@
 
 #include "../mySupervisedClassifier/mySupervisedClassifier.h"
 
-class myAdaBoost : public mySupervisedClassifier {
+class myAdaBoost final : public mySupervisedClassifier {
 public:
-    myAdaBoost();
-    virtual ~myAdaBoost();
+    myAdaBoost(unsigned int iWeakCount = 100);
+
+    virtual ~myAdaBoost(void);
+
+    void Load(const std::string& sFilePath) override {
+        m_poClassifier = cv::ml::StatModel::load<cv::ml::Boost>(sFilePath);
+    }
+
+private:
+    // AdaBoost Types
+    // it can be the following values
+    // DISCRETE, REAL(default), LOGIT, GENTLE
+    static const int BOOST_TYPE = cv::ml::Boost::Types::REAL;
+
+    // A threshold between 0 and 1 used to save computational time
+    // training will stop when samples with summary weight <= 1 - TRIM_RATE
+    // default value is 0.95, 0 for turning off
+    static const double TRIM_RATE;
+
+private:
+
 };
 
 #endif // !_MY_ADABOOST_H_
