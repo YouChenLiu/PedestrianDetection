f75faaa51c8908ba2f67b59adbbc74cef0c18f04
YouChen Liu <youchenliu@outlook.com>
2016-02-04 21:26:10 +0800

    remove header add codes for using mySVM and myAdaBoost

--- test/Source.cpp
+++ test/Source.cpp
@@ -1,7 +1,7 @@
-#include <opencv2/core.hpp>
 #include "../myLibrary/myClassifier/myAdaBoost/myAdaBoost.h"
 
 int main(void) {
+    //mySVM oClassifier();
     myAdaBoost oClassifier(5);
     for (std::size_t i = 0; i < 10; ++i) {
         oClassifier.AddSample(+1, std::vector<float>(10, static_cast<float>(i)));
@@ -13,7 +13,7 @@ int main(void) {
     oClassifier.LoadFeature("test.txt");
     auto result = oClassifier.Predict(std::vector<float>(10, -1.1f));
     oClassifier.Save("test.xml");
-    //oClassifier.Load("test.xml");
+    oClassifier.Load("test.xml");
     result = oClassifier.Predict(std::vector<float>(10, 1.1f));
 	return 0;
 }
