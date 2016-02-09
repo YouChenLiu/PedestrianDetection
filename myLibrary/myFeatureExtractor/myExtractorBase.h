65b74e300d76e438806bca5292026e97825931b3
YouChen Liu NB <youchenliu@outlook.com>
2016-02-09 22:20:00 +0800

    add common header modify the coding style for public, protected and private access label

--- myLibrary/myFeatureExtractor/myExtractorBase.h
+++ myLibrary/myFeatureExtractor/myExtractorBase.h
@@ -1,17 +1,37 @@
 #ifndef _MY_EXTRACTOR_BASE_H_
 #define _MY_EXTRACTOR_BASE_H_
 
-#include <opencv2/core.hpp>
+#include "../common.h"
 
 class myExtractorBase {
+public:     // public attribute
+    // define the different pattern for the derived extractor
+    class myFeatureBase {
     public:
-        class myFeatureBase {
-        public:
-            static const int NONE = -1;
-        };
+        static const int NONE = -1;
+    };
 
-        virtual void Describe(cv::Point2i, std::vector<float>&) const = 0;
-        virtual ~myExtractorBase(void) {}
+protected:  // protected attribute
+    // the image for extracting feature
+    cv::Mat m_mImage;
+
+private:    // private attribute
+
+public:     // public method
+    myExtractorBase(void);
+    myExtractorBase(const cv::Mat& mImage);
+    virtual ~myExtractorBase(void);
+
+    // pure virtual function for describe the feature at sepecify position
+    virtual void Describe(cv::Point2i, std::vector<float>&) const = 0;
+
+    // setting the image want to extract feature
+    void SetImage(const cv::Mat& mImage) { m_mImage = mImage; }
+
+protected:  // protectd method
+
+private:    // private method
+
 };
 
 #endif
