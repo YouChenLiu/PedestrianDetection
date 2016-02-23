bb68f7239d77980d27871cc62c23a781972f9e14
YouChen Liu <youchenliu@outlook.com>
2016-02-23 21:26:39 +0800

    complete the SaveModels method

--- myLibrary/myModelCollector/myModelCollector.cpp
+++ myLibrary/myModelCollector/myModelCollector.cpp
@@ -1,38 +1,83 @@
 #include "myModelCollector.h"
+#include <iomanip>
+#include <fstream>
+
+unsigned int myModelCollector::m_iCount = 0;
 
 myModelCollector::myModelCollector(void) {
+    Init();
 }
 
 myModelCollector::myModelCollector(unsigned int iNumOfModel) {
-    m_vpoModel.resize(iNumOfModel);
+    Init();
+    Resize(iNumOfModel);
 }
 
-myModelCollector::myModelCollector(
-    const cv::Mat & mImage, std::unique_ptr<myModelIndexerBase> poIndexer) {
-    m_mImage = mImage;
-    m_poIndexer = std::move(poIndexer);
-    m_vpoModel.resize(m_poIndexer->GetNumOfBins());
+myModelCollector::~myModelCollector(void) {
+    --m_iCount;
 }
 
-myModelCollector::~myModelCollector(void) {}
+void myModelCollector::Resize(unsigned int iCount) {
+    m_vpoModel.clear();
+    m_vpoModel.reserve(iCount);
+    for (size_t i = 0; i < iCount; ++i) {
+        m_vpoModel.push_back(std::make_unique<mySVM>());
+    }
+}
 
 void myModelCollector::TrainModels(void) {
-    for (auto& model : m_vpoModel) {
+    for (const auto& model : m_vpoModel) {
         model->Train();
     }
 }
 
-void myModelCollector::SaveModels(void) {
+void myModelCollector::SaveModels(const std::string& sRootPath) const {
+    // buffer for folder string
+    std::stringstream ssFolder;
+    ssFolder << std::setfill('0') << std::setw(4) << m_iSN;
+
+    // buffer for path string
+    std::stringstream ssPath;
+    ssPath << sRootPath << "/" << ssFolder.str();
+    
+    // buffer for command string
+    std::stringstream ssCommand;
+    ssCommand << "mkdir " << "\"" << ssPath.str() << "\"";
+    std::string t = ssCommand.str();
+    
+    // create a sub folder at sRootPath
+    std::system(ssCommand.str().c_str());
+
+    // open file for write the xml file list
+    std::ofstream FileList(sRootPath + "/" + "list.txt", std::ios_base::app);
+
+    // add a line for xml file list
+    FileList << ssFolder.str() << "/" << "list.txt" << std::endl;
+
+    // open the file list in the sub folder, then write out the xml file path
+    FileList.close();
+    FileList.open(ssPath.str() + "/" + "list.txt");
+
+    // write out the svm models
+    for (std::size_t i = 0; i < m_vpoModel.size(); i++) {
+        // buffer for file name
+        std::stringstream ssFileName;
+        ssFileName << std::setfill('0') << std::setw(3) << i << ".xml";
+
+        // create the path for saving model
+        std::stringstream ssXMLPath;
+        ssXMLPath << ssPath.str() << "/" << ssFileName.str();
+
+        // save the model and add a line in file list
+        if (m_vpoModel.at(i)->Save(ssXMLPath.str())) {
+            FileList << ssFileName.str() << std::endl;
+        }
+    }
 }
 
 void myModelCollector::LoadModels(std::string sFilePath) {
-}
+    
 
-void myModelCollector::AddSample(cv::Point2i ptPosition, int iLabel) {
-    auto BinNumber = m_poIndexer->GetBinNumber(ptPosition);
-    std::vector<float> vfFeature;
-    m_poExtractor->Describe(ptPosition, vfFeature);
-    AddSample(BinNumber, iLabel, vfFeature);
 }
 
 void myModelCollector::AddSample(unsigned int iBinNumber,
@@ -42,4 +87,5 @@ void myModelCollector::AddSample(unsigned int iBinNumber,
 }
 
 void myModelCollector::Init(void) {
+    m_iSN = m_iCount++;
 }
--- myLibrary/myModelCollector/myModelCollector.h
+++ myLibrary/myModelCollector/myModelCollector.h
@@ -1,10 +1,10 @@
 #ifndef _MY_MODEL_COLLECTOR_H_
 #define _MY_MODEL_COLLECTOR_H_
 
-#include "../common.h"
 #include "myModelCollectorBase.h"
+#include "../common.h"
 #include "../myClassifier/mySVM/mySVM.h"
-#include "../myFeatureExtractor/myFeatureExtractor.h"
+#include <memory>
 
 class myModelCollector final : public myModelCollectorBase {
 public:     // public attribute
@@ -12,36 +12,35 @@ public:     // public attribute
 protected:  // protected attribute
 
 private:    // private attribute
+    // serial number
+    int m_iSN;
+
+    // total count
+    static unsigned int m_iCount;
+
     // boolean for saving the collector is created from file or not
     bool m_bCreatingFromFile;
 
     // array for saving models such as svm
     std::vector<std::unique_ptr<mySVM>> m_vpoModel;
 
-    std::unique_ptr<myFeatureExtractor> m_poExtractor;
-    cv::Mat m_mImage;
-
 public:     // public method
     myModelCollector(void);
     myModelCollector(unsigned int iNumOfModel);
-    myModelCollector(const cv::Mat& mImage,
-                     std::unique_ptr<myModelIndexerBase> poIndexer);
     virtual ~myModelCollector(void);
 
+    void Resize(unsigned int iCount);
     void TrainModels(void);
-    void SaveModels(void);
+    void SaveModels(const std::string& sRootPath) const;
     void LoadModels(std::string sFilePath);
-    void AddSample(cv::Point2i ptPosition, int iLabel);
-    myExtractorBase* GetExtractor(void) {
-        return m_poExtractor.get();
-    }
+    void AddSample(unsigned int iBinNumber, int iLabel,
+                   const std::vector<float>& vfFeature);
 
 protected:  // protected method
 
 private:    // private method
     void Init(void);
-    void AddSample(unsigned int iBinNumber, int iLabel,
-                   const std::vector<float>& vfFeature);
+
 };
 
 
