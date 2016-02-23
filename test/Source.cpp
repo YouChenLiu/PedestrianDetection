73f22b7be0b815a7b225917470d9e34f634862f3
YouChen Liu <youchenliu@outlook.com>
2016-02-23 21:28:17 +0800

    test the collector

--- test/Source.cpp
+++ test/Source.cpp
@@ -1,21 +1,41 @@
 #include "../myLibrary/myClassifier/mySVM/mySVM.h"
 #include "../myLibrary/myModelCollector/myModelCollector.h"
+#include "../myLibrary/myFeatureExtractor/myFeatureExtractor.h"
 #include "../myLibrary/myModelIndexer/myLBPIndexer/myLBPIndexer.h"
+#include "../myLibrary/myImageSequence/myImageSequence.h"
 #include <memory>
+#include <opencv2/highgui.hpp>
+#include "../myLibrary/myClassifier/mySVM/mySVM.h"
+#include <opencv2/ml.hpp>
 
 int main(void) {
     /*
-    mySVM oClassifier;
-    oClassifier.AddSample(+1, std::vector<float>(10, +1.0f));
-    oClassifier.AddSample(-1, std::vector<float>(10, -1.0f));
-
-    oClassifier.SaveFeatures("feature.txt");
+    cv::Size2i BlockSize(8, 8);
+    cv::Mat mImage = cv::imread("000001.bmp", cv::IMREAD_GRAYSCALE);
+    auto poExtractor = std::make_unique<myFeatureExtractor>(mImage, cv::Size2i(8, 8));
+    poExtractor->EnableFeature(myFeatureExtractor::Features::HOG_WITH_L2_NORM);
+    poExtractor->EnableFeature(myFeatureExtractor::Features::LBP_8_1_UNIFORM);
+    auto poCollector = std::make_unique<myModelCollector>(59);
+    auto poIndexer = std::make_unique<myLBPIndexer>();
+    cv::Point2i pt(8, 8);
+    std::vector<float> vfFeature;
+    auto iBinNumber = poIndexer->GetBinNumber(mImage, pt);
+    poExtractor->Describe(pt, vfFeature);
+    poCollector->AddSample(iBinNumber, 1, vfFeature);
     */
 
-    cv::Mat mImage = cv::Mat::zeros(64, 64, CV_8UC1);
-    auto poIndexer = std::make_unique<myModelIndexerBase>(new myLBPIndexer(mImage));
-    myModelCollector oController(mImage, std::move(poIndexer));
-    auto poExtractor = oController.GetExtractor();
-    poExtractor->SetImage(mImage);
+    std::array<myModelCollector, 5> aoCollector;
+
+    const std::string sRoot = "Models";
+    int i = 0;
+    for (auto& o : aoCollector) {
+        o.Resize(5);
+        o.AddSample(i, +1, std::vector<float>(10, +1.0f));
+        o.AddSample(i, -1, std::vector<float>(10, -1.0f));
+        o.TrainModels();
+        o.SaveModels(sRoot);
+        i++;
+    }
+    
     return 0;
 }
