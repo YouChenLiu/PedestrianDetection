4bca11112d49af5048a77d04233bb038883222ac
YouChen Liu <youchenliu@outlook.com>
2016-04-20 00:37:24 +0800

    add classifier header file for including all classifier class

---
+++ myLibrary/myClassifier.h
@@ -0,0 +1,13 @@
+/**
+ * @file myClassifier.h
+ * @brief include all classifiers
+ */
+
+#ifndef _MY_CLASSIFIER_H_
+#define _MY_CLASSIFIER_H_
+
+#include "myClassifier\mySupervisedClassifier\mySupervisedClassifier.h"
+#include "myClassifier\myAdaBoost\myAdaBoost.h"
+#include "myClassifier\mySVM\mySVM.h"
+
+#endif // !_MY_CLASSIFIER_H_
