c1b00ee773a35a9c02b0b67d23c87ed0b83037b3
YouChen Liu <youchenliu@outlook.com>
2016-05-12 18:05:26 +0800

    change some style

--- myLibrary/myResultVerifier/myResultVerifier.cpp
+++ myLibrary/myResultVerifier/myResultVerifier.cpp
@@ -2,155 +2,153 @@
 #include "../myLabel/myLabel.h"
 
 myResultVerifier::myResultVerifier(const std::string sDetctionResult, const std::string sGroundTruth) {
-    Init();
-    LoadXMLFile(sDetctionResult, sGroundTruth);
+  Init();
+  LoadXMLFile(sDetctionResult, sGroundTruth);
 }
 
 myResultVerifier::~myResultVerifier(void) {}
 
 void myResultVerifier::Init(void) {
-    m_poDetectionResultXMLFile = std::make_unique<tinyxml2::XMLDocument>();
-    m_poGroundTruthXMLFile = std::make_unique<tinyxml2::XMLDocument>();
-    m_fTruePositiveScore = m_fFalsePositiveScore = m_fTrueNegativeScore = m_fFalseNegativeScore = 0.0f;
-    m_iTotalFrames = 0;
+  m_poDetectionResultXMLFile = std::make_unique<tinyxml2::XMLDocument>();
+  m_poGroundTruthXMLFile = std::make_unique<tinyxml2::XMLDocument>();
+  m_fTruePositiveScore = m_fFalsePositiveScore = m_fTrueNegativeScore = m_fFalseNegativeScore = 0.0f;
+  m_iTotalFrames = 0;
 }
 
 void myResultVerifier::LoadXMLFile(const std::string sDetctionResult, const std::string sGroundTruth) {
-    m_poDetectionResultXMLFile->LoadFile(sDetctionResult.c_str());
-    m_poGroundTruthXMLFile->LoadFile(sGroundTruth.c_str());
+  m_poDetectionResultXMLFile->LoadFile(sDetctionResult.c_str());
+  m_poGroundTruthXMLFile->LoadFile(sGroundTruth.c_str());
 }
 
 float myResultVerifier::GetDetectionRate(void) {
-    float fDetectionRate = 0.0f;
+  float fDetectionRate = 0.0f;
 
-    if ((m_fTruePositiveScore + m_fFalsePositiveScore + m_fTrueNegativeScore + m_fFalseNegativeScore) == 0.0f) {
-        CompareXMLResult();
-    }
+  if ((m_fTruePositiveScore + m_fFalsePositiveScore + m_fTrueNegativeScore + m_fFalseNegativeScore) == 0.0f) {
+    CompareXMLResult();
+  }
 
-    fDetectionRate = m_fTruePositiveScore / (m_fTruePositiveScore + m_fFalseNegativeScore);
+  fDetectionRate = m_fTruePositiveScore / (m_fTruePositiveScore + m_fFalseNegativeScore);
 
-    return fDetectionRate;
+  return fDetectionRate;
 }
 
 void myResultVerifier::CompareXMLResult(void) {
-    while (CalculateDetectionRatePerFrame(m_iTotalFrames) == true) {
-        ++m_iTotalFrames;
-    }
+  while (CalculateDetectionRatePerFrame(m_iTotalFrames) == true) {
+    ++m_iTotalFrames;
+  }
 
-    m_iTotalFrames -= 1;
+  m_iTotalFrames -= 1;
 
-#   ifndef NDEBUG
-    std::cout << "Total Frames = " << m_iTotalFrames << std::endl;
-    std::cout << "TP Score = " << m_fTruePositiveScore << std::endl;
-    std::cout << "FP Score = " << m_fFalsePositiveScore << std::endl;
-    std::cout << "TN Score = " << m_fTrueNegativeScore << std::endl;
-    std::cout << "FN Score = " << m_fFalseNegativeScore << std::endl;
-#   endif
+# ifndef NDEBUG
+  std::cout << "Total Frames = " << m_iTotalFrames << std::endl;
+  std::cout << "TP Score = " << m_fTruePositiveScore << std::endl;
+  std::cout << "FP Score = " << m_fFalsePositiveScore << std::endl;
+  std::cout << "TN Score = " << m_fTrueNegativeScore << std::endl;
+  std::cout << "FN Score = " << m_fFalseNegativeScore << std::endl;
+# endif
 }
 
 bool myResultVerifier::CalculateDetectionRatePerFrame(int iFrameNumber) {
-    using namespace tinyxml2;
+  using namespace tinyxml2;
 
-    XMLElement* poDetectionDataSet = m_poDetectionResultXMLFile->RootElement()->FirstChildElement(myLabel::GetLabel(myLabel::Tags::DATA_SET).c_str());
-    XMLElement* poGroundTruthDataSet = m_poGroundTruthXMLFile->RootElement()->FirstChildElement(myLabel::GetLabel(myLabel::Tags::DATA_SET).c_str());
+  auto poDetectionData = m_poDetectionResultXMLFile->RootElement()->FirstChildElement(myLabel::GetLabel(myLabel::Tags::DATA_SET).c_str());
+  auto poGTData = m_poGroundTruthXMLFile->RootElement()->FirstChildElement(myLabel::GetLabel(myLabel::Tags::DATA_SET).c_str());
 
-    XMLElement* poDetectionHeader = poDetectionDataSet->FirstChildElement(myLabel::GetLabel(myLabel::Tags::HEADER).c_str());
-    while (poDetectionHeader != nullptr) {
-        int iFrame = poDetectionHeader->IntAttribute(myLabel::GetLabel(myLabel::Attributes::FRAME_NUMBER).c_str());
-        if (iFrame >= iFrameNumber) {
-            if (iFrame != iFrameNumber) {
-                poDetectionHeader = nullptr;
-            }
-            break;
-        }
-        poDetectionHeader = poDetectionHeader->NextSiblingElement();
+  auto poDetectionHeader = poDetectionData->FirstChildElement(myLabel::GetLabel(myLabel::Tags::HEADER).c_str());
+  while (poDetectionHeader != nullptr) {
+    int iFrame = poDetectionHeader->IntAttribute(myLabel::GetLabel(myLabel::Attributes::FRAME_NUMBER).c_str());
+    if (iFrame >= iFrameNumber) {
+      if (iFrame != iFrameNumber) {
+        poDetectionHeader = nullptr;
+      }
+      break;
     }
-    
-    XMLElement* poGroundTruthHeader = poGroundTruthDataSet->FirstChildElement(myLabel::GetLabel(myLabel::Tags::HEADER).c_str());
-    while (poGroundTruthHeader != nullptr) {
-        int iFrame = poGroundTruthHeader->IntAttribute(myLabel::GetLabel(myLabel::Attributes::FRAME_NUMBER).c_str());
-        if (iFrame >= iFrameNumber) {
-            if (iFrame != iFrameNumber) {
-                poDetectionHeader = nullptr;
-            }
-            break;
-        }
-        poGroundTruthHeader = poGroundTruthHeader->NextSiblingElement();
+    poDetectionHeader = poDetectionHeader->NextSiblingElement();
+  }
+
+  auto poGTHeader = poGTData->FirstChildElement(myLabel::GetLabel(myLabel::Tags::HEADER).c_str());
+  while (poGTHeader != nullptr) {
+    int iFrame = poGTHeader->IntAttribute(myLabel::GetLabel(myLabel::Attributes::FRAME_NUMBER).c_str());
+    if (iFrame >= iFrameNumber) {
+      if (iFrame != iFrameNumber) {
+        poDetectionHeader = nullptr;
+      }
+      break;
     }
+    poGTHeader = poGTHeader->NextSiblingElement();
+  }
 
-    if ((poDetectionHeader == nullptr) || (poGroundTruthHeader == nullptr)) {
-        return false;
-    }
+  if ((poDetectionHeader == nullptr) || (poGTHeader == nullptr)) {
+    return false;
+  }
 
-    std::vector<myBoundingBox> voDetectionBoxes;
-    TakeOutBoundingBox(poDetectionHeader, voDetectionBoxes);
-    std::vector<myBoundingBox> voGroundTruthBoxes;
-    TakeOutBoundingBox(poGroundTruthHeader, voGroundTruthBoxes);
+  std::vector<cv::Rect2i> voDetectionBoxes;
+  TakeOutBoundingBox(poDetectionHeader, voDetectionBoxes);
+  std::vector<cv::Rect2i> voGroundTruthBoxes;
+  TakeOutBoundingBox(poGTHeader, voGroundTruthBoxes);
 
-    if (static_cast<int>(voDetectionBoxes.size()) == 0) {
-        //if one of xml files has no corresponding records
-        return true;
-    }
-
-    if (static_cast<int>(voGroundTruthBoxes.size()) == 0) {
-        return true;
-    }
-
-    for (int iDetectionIndex = 0; iDetectionIndex < static_cast<int>(voDetectionBoxes.size()); ++iDetectionIndex) {
-        myBoundingBox DetectionBox = voDetectionBoxes.at(iDetectionIndex);
-        float fMaxOverlapRatio = 0.0f;
-        for (int iGTIndex = 0; iGTIndex < static_cast<int>(voGroundTruthBoxes.size()); ++iGTIndex) {
-            myBoundingBox GTBox = voGroundTruthBoxes.at(iGTIndex);
-            float fOverlapRatio = CalculateOverlapRatio(DetectionBox, GTBox);
-            if (fOverlapRatio > fMaxOverlapRatio) {
-                fMaxOverlapRatio = fOverlapRatio;
-            }
-        }
-        m_fTruePositiveScore += fMaxOverlapRatio;
-        m_fFalsePositiveScore += 1 - fMaxOverlapRatio;
-        m_fFalseNegativeScore += 1 - fMaxOverlapRatio;
-    }
-
+  if (static_cast<int>(voDetectionBoxes.size()) == 0) {
+    //if one of xml files has no corresponding records
     return true;
+  }
+
+  if (static_cast<int>(voGroundTruthBoxes.size()) == 0) {
+    return true;
+  }
+
+  for (int iDetectionIndex = 0; iDetectionIndex < static_cast<int>(voDetectionBoxes.size()); ++iDetectionIndex) {
+    auto DetectionBox = voDetectionBoxes.at(iDetectionIndex);
+    float fMaxOverlapRatio = 0.0f;
+    for (int iGTIndex = 0; iGTIndex < static_cast<int>(voGroundTruthBoxes.size()); ++iGTIndex) {
+      auto GTBox = voGroundTruthBoxes.at(iGTIndex);
+      float fOverlapRatio = CalculateOverlapRatio(DetectionBox, GTBox);
+      if (fOverlapRatio > fMaxOverlapRatio) {
+        fMaxOverlapRatio = fOverlapRatio;
+      }
+    }
+    m_fTruePositiveScore += fMaxOverlapRatio;
+    m_fFalsePositiveScore += 1 - fMaxOverlapRatio;
+    m_fFalseNegativeScore += 1 - fMaxOverlapRatio;
+  }
+
+  return true;
 }
 
-inline
-void myResultVerifier::TakeOutBoundingBox(tinyxml2::XMLElement* poHeader, std::vector<myBoundingBox>& voBoundingBoxes) const {
-    using namespace tinyxml2;
+void myResultVerifier::TakeOutBoundingBox(tinyxml2::XMLElement* poHeader, std::vector<cv::Rect2i>& voBoundingBoxes) const {
+  using namespace tinyxml2;
 
-    voBoundingBoxes.clear();
+  voBoundingBoxes.clear();
 
-    XMLElement* poRecord = poHeader->FirstChildElement(myLabel::GetLabel(myLabel::Tags::RECORD).c_str());
-    while (poRecord != nullptr) {
-        const std::string s = myLabel::GetLabel(myLabel::Tags::START_POINT);
-        XMLElement* Element = poRecord->FirstChildElement(myLabel::GetLabel(myLabel::Tags::START_POINT).c_str());
-        const std::string sStartPoint = Element->GetText();
-        const int iCommaIndex = static_cast<int>(sStartPoint.find(", "));
-        const int x = stoi(sStartPoint.substr(0, iCommaIndex));
-        const int y = stoi(sStartPoint.substr(iCommaIndex + 1, sStartPoint.size()));
-        const int iWidth = std::stoi(poRecord->FirstChildElement(myLabel::GetLabel(myLabel::Tags::WIDTH).c_str())->GetText());
-        const int iHeight = std::stoi(poRecord->FirstChildElement(myLabel::GetLabel(myLabel::Tags::HEIGHT).c_str())->GetText());
+  XMLElement* poRecord = poHeader->FirstChildElement(myLabel::GetLabel(myLabel::Tags::RECORD).c_str());
+  while (poRecord != nullptr) {
+    const std::string s = myLabel::GetLabel(myLabel::Tags::START_POINT);
+    XMLElement* Element = poRecord->FirstChildElement(myLabel::GetLabel(myLabel::Tags::START_POINT).c_str());
+    const std::string sStartPoint = Element->GetText();
+    const int iCommaIndex = static_cast<int>(sStartPoint.find(", "));
+    const int x = stoi(sStartPoint.substr(0, iCommaIndex));
+    const int y = stoi(sStartPoint.substr(iCommaIndex + 1, sStartPoint.size()));
+    const int iWidth = std::stoi(poRecord->FirstChildElement(myLabel::GetLabel(myLabel::Tags::WIDTH).c_str())->GetText());
+    const int iHeight = std::stoi(poRecord->FirstChildElement(myLabel::GetLabel(myLabel::Tags::HEIGHT).c_str())->GetText());
 
-        const myBoundingBox box = { x, y, iWidth, iHeight };
-        voBoundingBoxes.push_back(box);
-        poRecord = poRecord->NextSiblingElement();
-    }
+    const cv::Rect2i box(x, y, iWidth, iHeight);
+    voBoundingBoxes.push_back(box);
+    poRecord = poRecord->NextSiblingElement();
+  }
 }
 
-inline
-float myResultVerifier::CalculateOverlapRatio(myResultVerifier::myBoundingBox Detection, myResultVerifier::myBoundingBox GroundTruth) {
-    const int iDetectionArea = Detection.height * Detection.width;
-    const int iGroundTruthArea = GroundTruth.height * GroundTruth.width;
-
-    int iOverlapArea = 0;
-    for (int y = Detection.y; y < Detection.y + Detection.height; ++y) {
-        for (int x = Detection.x; x < Detection.x + Detection.width; ++x) {
-            if ((GroundTruth.y <= y && y < GroundTruth.y + GroundTruth.height) && 
-                (GroundTruth.x <= x && x < GroundTruth.x + GroundTruth.width)) {
-                ++iOverlapArea;
-            }
-        }
+float myResultVerifier::CalculateOverlapRatio(cv::Rect2i Detection, cv::Rect2i GroundTruth) {
+  const auto iDetectionArea = Detection.area();
+  const auto iGroundTruthArea = GroundTruth.area();
+  
+  int iOverlapArea = 0;
+  for (int y = Detection.y; y < Detection.y + Detection.height; ++y) {
+    for (int x = Detection.x; x < Detection.x + Detection.width; ++x) {
+      cv::Point2i pt(x, y);
+      if (GroundTruth.contains(pt)) {
+        ++iOverlapArea;
+      }
     }
+  }
 
-    return (float)iOverlapArea / (float)(iDetectionArea + iGroundTruthArea - iOverlapArea);
+  return static_cast<float>(iOverlapArea) / (iDetectionArea + iGroundTruthArea - iOverlapArea);
 }
--- myLibrary/myResultVerifier/myResultVerifier.h
+++ myLibrary/myResultVerifier/myResultVerifier.h
@@ -16,93 +16,90 @@
  */
 class myResultVerifier {
 public:     // public attribute
-    /**
-     * @brief A simple bounding box definition.
-     */
-    struct myBoundingBox {
-        int x;          //!< Left-top point X
-        int y;          //!< Left-top point Y
-        int width;      //!< Bounding box width
-        int height;     //!< Bounfing box height
-    };
-
+  /**
+   * @brief 
+   */
+  enum class Method {
+    GYC,
+    FPPW
+  };
 protected:  // protected attribute
 
 private:    // private attribute
-    float m_fTruePositiveScore;
-    float m_fFalsePositiveScore;
-    float m_fTrueNegativeScore;
-    float m_fFalseNegativeScore;
-    int m_iTotalFrames;
-    std::unique_ptr<tinyxml2::XMLDocument> m_poDetectionResultXMLFile;
-    std::unique_ptr<tinyxml2::XMLDocument> m_poGroundTruthXMLFile;
+  float m_fTruePositiveScore;
+  float m_fFalsePositiveScore;
+  float m_fTrueNegativeScore;
+  float m_fFalseNegativeScore;
+  int m_iTotalFrames;
+  std::unique_ptr<tinyxml2::XMLDocument> m_poDetectionResultXMLFile;
+  std::unique_ptr<tinyxml2::XMLDocument> m_poGroundTruthXMLFile;
 
 public:     // public method
-    /**
-     * @brief Default constructor.
-     */
-    myResultVerifier(void) { Init(); }
+  /**
+   * @brief Default constructor.
+   */
+  myResultVerifier(void) { Init(); }
 
-    /**
-     * @brief Create by two XML file
-     *
-     * @param sDetctionResultPath The XML file want to compare.
-     * @param sGroundTruthPath The XML file saves the ground-truth.
-     */
-    myResultVerifier(const std::string sDetctionResultPath,
-                     const  std::string sGroundTruthPath);
-    ~myResultVerifier(void);
+  /**
+   * @brief Create by two XML file
+   *
+   * @param sDetctionResultPath The XML file want to compare.
+   * @param sGroundTruthPath The XML file saves the ground-truth.
+   */
+  myResultVerifier(const std::string sDetctionResultPath,
+                   const std::string sGroundTruthPath);
+  ~myResultVerifier(void);
 
-    /**
-     * @brief Load two XML file after create.
-     *
-     * @param sDetctionResultPath The XML file want to compare.
-     * @param sGroundTruthPath The XML file saves the ground-truth.
-     */
-    void LoadXMLFile(const std::string sDetctionResultPath,
-                     const  std::string sGroundTruthPath);
+  /**
+   * @brief Load two XML file after create.
+   *
+   * @param sDetctionResultPath The XML file want to compare.
+   * @param sGroundTruthPath The XML file saves the ground-truth.
+   */
+  void LoadXMLFile(const std::string sDetctionResultPath,
+                   const std::string sGroundTruthPath);
 
-    /**
-     * @brief Compare two XML file.
-     */
-    void CompareXMLResult(void);
+  /**
+   * @brief Compare two XML file.
+   */
+  void CompareXMLResult(void);
 
-    /**
-     * @brief calculate detection rate.
-     */
-    float GetDetectionRate(void);
-    
-#   ifndef NDEBUG
-    /**
-     * @brief Get true positive score
-     */
-    float GetTruePositiveScore(void) const { return m_fTruePositiveScore; }
-    
-    /**
-     * @brief Get false positive score
-     */
-    float GetFalsePositiveScore(void) const { return m_fFalsePositiveScore; }
-    
-    /**
-     * @brief Get true negative score
-     */
-    float GetTrueNegativeScore(void) const { return m_fTrueNegativeScore; }
-    
-    /**
-     * @brief Get false negative score
-     */
-    float GetFalseNegativeScore(void) const { return m_fFalseNegativeScore; }
-#   endif
+  /**
+   * @brief calculate detection rate.
+   */
+  float GetDetectionRate(void);
+
+# ifndef NDEBUG
+  /**
+   * @brief Get true positive score
+   */
+  float GetTruePositiveScore(void) const { return m_fTruePositiveScore; }
+
+  /**
+   * @brief Get false positive score
+   */
+  float GetFalsePositiveScore(void) const { return m_fFalsePositiveScore; }
+
+  /**
+   * @brief Get true negative score
+   */
+  float GetTrueNegativeScore(void) const { return m_fTrueNegativeScore; }
+
+  /**
+   * @brief Get false negative score
+   */
+  float GetFalseNegativeScore(void) const { return m_fFalseNegativeScore; }
+# endif
 
 protected:  // protected method
 
 private:    // private method
-    void Init(void);
-    float CalculateOverlapRatio(myBoundingBox Detection,
-                                myBoundingBox GroundTruth);
-    bool CalculateDetectionRatePerFrame(int iFrameNumber);
-    void TakeOutBoundingBox(tinyxml2::XMLElement* poHeader,
-                            std::vector<myBoundingBox>& voBoundingBoxes) const;
+  void Init(void);
+  float CalculateOverlapRatio(cv::Rect2i Detection,
+                              cv::Rect2i GroundTruth);
+  bool CalculateDetectionRatePerFrame(int iFrameNumber);
+  void TakeOutBoundingBox(tinyxml2::XMLElement* poHeader,
+                          std::vector<cv::Rect2i>& voBoundingBoxes) const;
 };
 
 #endif
